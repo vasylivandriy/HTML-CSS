cfdb9344672f4bcc1b1727a2b36e6e663131c75a
vasylivandriy <vasylivandriy@gmail.com>
2020-01-27 10:50:27 +0200

    8 Commit

--- ConditionsOperators/Task2Condition.js
+++ ConditionsOperators/Task2Condition.js
@@ -2,10 +2,9 @@ function task2Condition() {
 
     let name = prompt("Input your name, please");
 
-
     if (name == null) alert("Entry cenceled");
 
-    else if (name.replace(/[+*?$^(.)'"/[0123456789]]/g,"").length === 0  ) {
+    else if (name.replace(/[+*#@?!&%$^(:;.,№)'"/[0123456789]/g,"").length === 0  ) {
         alert("I don't know you");
 
     } else {
