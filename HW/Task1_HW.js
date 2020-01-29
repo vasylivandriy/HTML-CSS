9dc7a0987800acb86e2e6c3472a2fa8e07c13ad4
vasylivandriy <vasylivandriy@gmail.com>
2020-01-30 01:29:06 +0200

    4 commit

--- HW/Task1_HW.js
+++ HW/Task1_HW.js
@@ -1,15 +1,15 @@
-let deg = () => {
+let deg = function() {
 
-    let n = +prompt("Input number, please");
-    if (n !== +null) {
-        let k = +prompt("Input degree number, please");
-        if (k != null) {
-            if (n === 0) {
-                alert("A positive degree of number = " + Math.pow(n, k) + ", \nA negative degree of number = " + Math.pow(n, -k) + ", \nA zero degree of number isn't specified ");
-            } else {
-                alert("A positive degree of number = " + Math.pow(n, k) + ", \nA negative degree of number = " + Math.pow(n, -k) + ", \nA zero degree of number = " + Math.pow(n, 0) + " ");
-            }
-        } else alert('ERROR!!! You must input two numbers. ');
+    let n = prompt("Input number, please");
+    if (n != null || n.replace(/[-0123456789]/g,"" ).length===0) { alert('');
+        // let k = +prompt("Input degree number, please");
+        // if (k != null) {
+        //     if (n === 0) {
+        //         alert("A positive degree of number = " + Math.pow(n, k) + ", \nA negative degree of number = " + Math.pow(n, -k) + ", \nA zero degree of number isn't specified ");
+        //     } else {
+        //         alert("A positive degree of number = " + Math.pow(n, k) + ", \nA negative degree of number = " + Math.pow(n, -k) + ", \nA zero degree of number = " + Math.pow(n, 0) + " ");
+        //     }
+        // } else alert('ERROR!!! You must input two numbers. ');
     } else alert('ERROR!!! You must input two numbers. ');
 
 };
