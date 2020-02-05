106ad06b17e837414512ba9611f501d024c3bb28
vasylivandriy <vasylivandriy@gmail.com>
2020-02-05 15:03:36 +0200

    3 commit

---
+++ BonusPlus/Task1/Task1JS.js
@@ -0,0 +1,16 @@
+// window.onload = function () {
+
+   // let  divOne = prompt('Input code of a color for div1');
+    // let divTwo = prompt('Input code of a color for div2');
+    // let divThree = prompt('Input code of a color for div3');
+
+
+
+//         document.getElementsByClassName('box')[0].style.backgroundColor = 'red';
+//
+//
+//
+//
+//
+//
+// };
